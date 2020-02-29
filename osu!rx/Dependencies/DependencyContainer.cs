f357a71d0f9a3d0cc584e475033a25a8ed2c008d
mrflashstudio <mrflashstudio@gmail.com>
2020-02-29 19:53:54 +0300

    Implement simple DI for future use

---
+++ osu!rx/Dependencies/DependencyContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace osu_rx.Dependencies
+{
+    public class DependencyContainer
+    {
+        private static List<object> dependencies = new List<object>();
+
+        public static void Cache<T>(T dependency) => dependencies.Add(dependency);
+
+        public static T Get<T>()
+        {
+            var dependencyType = typeof(T);
+
+            var dependency = dependencies.Find(d => d.GetType() == dependencyType);
+            if (dependency != default)
+                return (T)dependency;
+            else if (dependency == null)
+                throw new Exception($"Dependency of type {dependencyType} not cached!");
+            else
+                throw new Exception($"Dependency of type {dependencyType} not found!");
+        }
+    }
+}
