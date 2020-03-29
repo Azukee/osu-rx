064e125d35599f1010646337f064695b8eeae1ea
mrflashstudio <mrflashstudio@gmail.com>
2020-03-29 12:49:37 +0300

    Use new config library

--- osu!rx/Configuration/ConfigManager.cs
+++ osu!rx/Configuration/ConfigManager.cs
@@ -1,91 +1,92 @@
-﻿using WindowsInput.Native;
+﻿using SimpleIniConfig;
+using WindowsInput.Native;
 
 namespace osu_rx.Configuration
 {
     public class ConfigManager
     {
-        private SimpleIniConfig config;
+        private Config config;
 
         public PlayStyles PlayStyle
         {
-            get => config.ReadValue<PlayStyles>("PlayStyle", PlayStyles.Singletap);
+            get => config.GetValue<PlayStyles>("PlayStyle", PlayStyles.Singletap);
             set => config.SetValue<PlayStyles>("PlayStyle", value);
         }
 
         public VirtualKeyCode PrimaryKey
         {
-            get => config.ReadValue<VirtualKeyCode>("PrimaryKey", VirtualKeyCode.VK_Z);
+            get => config.GetValue<VirtualKeyCode>("PrimaryKey", VirtualKeyCode.VK_Z);
             set => config.SetValue<VirtualKeyCode>("PrimaryKey", value);
         }
 
         public VirtualKeyCode SecondaryKey
         {
-            get => config.ReadValue<VirtualKeyCode>("SecondaryKey", VirtualKeyCode.VK_X);
+            get => config.GetValue<VirtualKeyCode>("SecondaryKey", VirtualKeyCode.VK_X);
             set => config.SetValue<VirtualKeyCode>("SecondaryKey", value);
         }
 
         public VirtualKeyCode HitWindow100Key
         {
-            get => config.ReadValue<VirtualKeyCode>("HitWindow100Key", VirtualKeyCode.SPACE);
+            get => config.GetValue<VirtualKeyCode>("HitWindow100Key", VirtualKeyCode.SPACE);
             set => config.SetValue<VirtualKeyCode>("HitWindow100Key", value);
         }
 
         public int MaxSingletapBPM
         {
-            get => config.ReadValue<int>("MaxSingletapBPM", 250);
+            get => config.GetValue<int>("MaxSingletapBPM", 250);
             set => config.SetValue<int>("MaxSingletapBPM", value);
         }
 
         public int AudioOffset
         {
-            get => config.ReadValue<int>("AudioOffset", 5);
+            get => config.GetValue<int>("AudioOffset", 0);
             set => config.SetValue<int>("AudioOffset", value);
         }
 
         public bool UseCustomWindowTitle
         {
-            get => config.ReadValue<bool>("UseCustomWindowTitle", false);
+            get => config.GetValue<bool>("UseCustomWindowTitle", false);
             set => config.SetValue<bool>("UseCustomWindowTitle", value);
         }
 
         public string CustomWindowTitle
         {
-            get => config.ReadValue<string>("CustomWindowTitle", string.Empty);
+            get => config.GetValue<string>("CustomWindowTitle", string.Empty);
             set => config.SetValue<string>("CustomWindowTitle", value);
         }
 
         public bool EnableHitScan
         {
-            get => config.ReadValue<bool>("EnableHitScan", true);
+            get => config.GetValue<bool>("EnableHitScan", true);
             set => config.SetValue<bool>("EnableHitScan", value);
         }
 
         //TODO: expose those in next update through settings if nothing changes
         public bool EnableHitScanPrediction
         {
-            get => config.ReadValue<bool>("H_EnablePrediction", true);
+            get => config.GetValue<bool>("H_EnablePrediction", true);
             set => config.SetValue<bool>("H_EnablePrediction", value);
         }
 
         public float HitScanRadiusMultiplier
         {
-            get => config.ReadValue<float>("H_RadiusMultiplier", 0.9f);
+            get => config.GetValue<float>("H_RadiusMultiplier", 0.9f);
             set => config.SetValue<float>("H_RadiusMultiplier", value);
         }
 
         public int HitScanRadiusAdditional
         {
-            get => config.ReadValue<int>("H_RadiusAdditional", 50);
+            get => config.GetValue<int>("H_RadiusAdditional", 50);
             set => config.SetValue<int>("H_RadiusAdditional", value);
         }
 
         public int HitScanMaxDistance
         {
-            get => config.ReadValue<int>("H_MaxDistance", 30);
+            get => config.GetValue<int>("H_MaxDistance", 30);
             set => config.SetValue<int>("H_MaxDistance", value);
         }
 
-        public ConfigManager() => config = new SimpleIniConfig();
+        public ConfigManager() => config = new Config();
     }
 
     public enum PlayStyles
